6498e05e8965302f04fe1e4a17aad693f3b7a8c8
lee <sipossa@naver.com>
2022-12-29 18:40:03 +0900

    20221229

--- js/main.js
+++ js/main.js
@@ -1,51 +1,38 @@
 $(function () {
 
     // top banner
-
-    const topBammerLInk = $('.top_banner i');
-    topBannerClose = () => {
+    $('.top_banner i').on('click', function () {
         $('.top_banner').addClass('on');
-    }
-    topBammerLInk.on('click', topBannerClose);
+    });
 
     // mainSlide  
 
-    const mainSlideItm = $('.main_slide');
-    const slideMainEvent = (e, s, c) => {
-        const current = $('.main_slide .slick-current');
+    $('.main_slide').on('init afterChange', function (e, s, c) {
+        var current = $('.main_slide .slick-current');
         current.addClass('on').siblings().removeClass('on');
         $('.mainVisual .main_slide_num span').text(c ? (c + 1) : 1);
         $('.mainVisual .main_slide_num strong').text(s.slideCount);
-    }
+    });
 
-    const slideDefaultOption = {
+    $('.main_slide').slick({
         arrows: false,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 4000,
         pauseOnHover: false,
         pauseOnFocus: false,
-    }
+    })
 
-    mainSlideItm.on('init', slideMainEvent);
-    const mainSlide = mainSlideItm.slick({ ...slideDefaultOption, autoplaySpeed: 4000 });
-    mainSlide.on('afterChange', slideMainEvent);
+    $('.mainVisual .left_arrow').on('click', function () {
+        $('.main_slide').slick('slickPrev')
+    });
+    $('.mainVisual .right_arrow').on('click', function () {
+        $('.main_slide').slick('slickNext')
+    });
 
-    const slideLeftArrow = $('.mainVisual .left_arrow');
-    const slideRightArrow = $('.mainVisual .right_arrow');
-
-    slideLeftArrow.on('click', () => { mainSlide.slick('slickPrev') });
-    slideRightArrow.on('click', () => { mainSlide.slick('slickNext') });
-
-    const slideDots = $('.mainVisual .main_slide_dots button');
-
-    slideDotsLink = e => {
-        const target = e.target;
-        const idx = $(target).parent().index();
-        //const idx = $(this).parent().index();
-        mainSlide.slick('slickGoTo', idx);
-    }
-
-    slideDots.on('click', slideDotsLink);
+    $('.mainVisual .main_slide_dots button').on('click', function () {
+        var idx = $(this).parent().index();
+        $('.main_slide').slick('slickGoTo', idx);
+    });
 
 
 
